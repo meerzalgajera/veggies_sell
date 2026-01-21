cbfbf5e67d8e1adfbf4800fb7008380c24badeb1
meerzalgajera <gajerameerzal@gmail.com>
2026-01-21 19:36:30 +0530

    Initial veggies sell website

---
+++ js/custom.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.main-banner-slider .silde6').owlCarousel({
+        loop:true,
+        nav:true,
+        dots:false,
+        items:1,
+        autoplay:true,
+       autoplayTimeout:1000,
+       autoplayHoverPause:true,
+    });
+});
